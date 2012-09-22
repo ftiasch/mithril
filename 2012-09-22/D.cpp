78a26f4906d8b5c8cbffee6014159db67336ee3d
Bin Jin <bjin@ctrl-d.org>
2012-09-23 00:51:45 +0800

    vim retab

--- 2012-09-22/D.cpp
+++ 2012-09-22/D.cpp
@@ -7,24 +7,24 @@ char number[16];
 
 int dfs(int step,long long sum,long long last,int sign)
 {
-	if (step==n){
-		sum+=last*sign;
-		return sum==0 && sign==-1;
-	}
-	
-	//=
-	int res=0;
-	if (sign==1){
-		res+=dfs(step+1,sum+last*sign,(number[step]-'0'),-1);
-	}
-	
-	//+
-	res+=dfs(step+1,sum+last*sign,(number[step]-'0'),sign);
-	
-	//concate
-	res+=dfs(step+1,sum,last*10+(number[step]-'0'),sign);
-	
-	return res;
+    if (step==n){
+        sum+=last*sign;
+        return sum==0 && sign==-1;
+    }
+    
+    //=
+    int res=0;
+    if (sign==1){
+        res+=dfs(step+1,sum+last*sign,(number[step]-'0'),-1);
+    }
+    
+    //+
+    res+=dfs(step+1,sum+last*sign,(number[step]-'0'),sign);
+    
+    //concate
+    res+=dfs(step+1,sum,last*10+(number[step]-'0'),sign);
+    
+    return res;
 }
 
 int main() {
